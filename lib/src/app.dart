2402c884400384833d9877c53b814c95a0e168ce
Qihao He <qi.he@maine.edu>
2019-07-28 20:19:23 -0700

    pics added container, text, pictures from url

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -8,6 +12,18 @@ class App extends StatefulWidget {
 
 class AppState extends State<App>{
   int counter = 0;
+  List<ImageModel> images = [];
+  final String url = 'http://jsonplaceholder.typicode.com/photos/';
+
+  void fetchImage() async {
+    counter++;
+    final response = await get(url + '$counter');
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   Widget build(context) {
     return MaterialApp(
@@ -17,13 +33,9 @@ class AppState extends State<App>{
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
         ),
-        body: Text('$counter'),
+        body: ImageList(images),
       ),
     );
   }
